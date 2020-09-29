ca3f51e0b85b7c162b7b0c7eb25a7e2d91859b56
JJZAPATAI <jjzapata1@uc.cl>
2020-09-29 00:28:35 -0300

    Validación de Salas y Manejo de Errores

--- gogym/src/models/room.js
+++ gogym/src/models/room.js
@@ -1,8 +1,26 @@
 module.exports = (sequelize, DataTypes) => {
   const room = sequelize.define('room', {
-    gym_id: DataTypes.INTEGER,
-    type: DataTypes.STRING,
-    capacity: DataTypes.INTEGER,
+    gym_id:{ 
+      type: DataTypes.INTEGER,
+    },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    capacity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        isNumeric: true,
+        isInt: true,
+        max: 30,
+        min: 5,
+      },
+    }
   }, {});
 
   room.associate = function associate() {
--- gogym/src/routes/room.js
+++ gogym/src/routes/room.js
@@ -2,8 +2,13 @@ const KoaRouter = require('koa-router');
 
 const router = new KoaRouter();
 
+const PERMITED_FIELDS =[
+    'type',
+    'capacity'
+];
+
 router.param('id', async (id, ctx, next) => {
-    const sala = await ctx.orm.sala.findByPk(id);
+    const sala = await ctx.orm.room.findByPk(id);
     if (!sala) ctx.throw(404);
     ctx.state.sala = sala;
     return next();
@@ -15,15 +20,35 @@ router.get('room', '/', async(ctx) => {
         room,
         newRoomPath: ctx.router.url('room-new'),
     });
-
-});
-
-router.get('sala', '/:id', (ctx) => {
-    const { sala } = ctx.state;
-    return ctx.render('room/show', { sala });
 });
 
 router.get ('room-new', '/new', (ctx) => {
-    return ctx.render('room/new')
+    const sala = ctx.orm.room.build();
+    return ctx.render('room/new', {
+        sala,
+        createRoomPath: ctx.router.url('room-create'),
+    });
 });
+
+router.post('room-create', '/', async (ctx) => {
+    const sala = ctx.orm.room.build(ctx.request.body);
+    try {
+        console.log(sala);
+    await sala.save({ fields: PERMITED_FIELDS });
+    ctx.redirect(ctx.router.url('room'));
+    } catch (error) {
+        await ctx.render('room/new', {
+            sala,
+            errors: error.errors,
+            createRoomPath: ctx.router.url('room-create'),
+        });
+    }
+});
+
+router.get('sala', '/:id', (ctx) => {
+    const {sala} = ctx.state;
+    return ctx.render('room/show', { sala });
+});
+
+
 module.exports = router;
