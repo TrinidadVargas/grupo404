9bd6fdcf595e011904fbb4dcc6926d97f090bff0
Daniela Villarroel <dpvillarroel@uc.cl>
2020-09-30 00:42:50 -0300

    faltadelete

--- gogym/src/routes.js
+++ gogym/src/routes.js
@@ -2,10 +2,12 @@ const KoaRouter = require('koa-router');
 
 const hello = require('./routes/hello');
 const index = require('./routes/index');
+const machines = require('./routes/machines');
 
 const router = new KoaRouter();
 
 router.use('/', index.routes());
 router.use('/hello', hello.routes());
+router.use('/machines',machines.routes());
 
 module.exports = router;
--- gogym/src/seeds/20200929140327-machines.js
+++ gogym/src/seeds/20200929140327-machines.js
@@ -88,6 +88,7 @@ module.exports = {
  
     
 
+
   },
 
   down: async (queryInterface, Sequelize) => {
